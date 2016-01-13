651acde4240e2f470410513436d263c6ca8722e1
gmfnasg <gpx_mark_farng@hotamil.com>
2016-01-13 21:46:48 +0800

    進度

---
+++ Look/Assets/ForceLook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceLook : MonoBehaviour {
+    public float WatingTime = 1;
+    public float CheckDoneTime = -1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
