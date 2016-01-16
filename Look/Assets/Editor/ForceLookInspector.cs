58e67e87055a22462c34f8ef3a68592befe05172
gmf <gpx_mark_farng@hotmail.com>
2016-01-16 19:36:45 +0800

    進度

---
+++ Look/Assets/Editor/ForceLookInspector.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor (typeof(ForceLook))]
+public class ForceLookInspector : Editor {
+
+	ForceLook ForceLook;
+
+	// Use this for initialization
+	void OnEnable () {
+		ForceLook = (ForceLook)target ;
+	}
+
+	public override void OnInspectorGUI(){
+
+		if (ForceLook == null)
+			return;
+		EditorGUILayout.BeginVertical ("Box");
+		EditorGUILayout.LabelField ("參數");
+		if(ForceLook.CheckDoneTime!=null)
+			EditorGUILayout.LabelField ("CheckDoneTime: " + ForceLook.CheckDoneTime);
+		EditorGUILayout.ObjectField ("ForceGo", ForceLook.ForceGo, typeof(GameObject)); 
+		EditorGUILayout.EndVertical ();
+
+		EditorGUILayout.BeginVertical ("Box");
+		EditorGUILayout.LabelField ("基本設定");
+		ForceLook.WatingTime = EditorGUILayout.FloatField ("WatingTime", ForceLook.WatingTime);
+		ForceLook.CardBoard = EditorGUILayout.ObjectField ("CardBoard", ForceLook.CardBoard, typeof(GameObject)) as GameObject; 
+		EditorGUILayout.EndVertical ();
+
+		EditorGUILayout.BeginVertical ("Box");
+		EditorGUILayout.LabelField ("Debug");
+		ForceLook.DebugInfoText = EditorGUILayout.ObjectField ("DebugInfoText", ForceLook.DebugInfoText, typeof(Text)) as Text; 
+		ForceLook.DebugInfoWatingTimeText = EditorGUILayout.ObjectField ("DebugInfoWatingTimeText", ForceLook.DebugInfoWatingTimeText, typeof(Text)) as Text; 
+		EditorGUILayout.EndVertical ();
+	}
+
+}
--- Look/Assets/ForceLook.cs
+++ Look/Assets/ForceLook.cs
@@ -1,17 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Collider))]
 public class ForceLook : MonoBehaviour {
     public float WatingTime = 1;
+	public GameObject CardBoard;
+
     public float CheckDoneTime = -1;
+	public GameObject ForceGo = null;
+
+	public static Text DebugInfoWatingTimeText;
+	public static Text DebugInfoText;
 
 	// Use this for initialization
 	void Start () {
-	
+		SetGazedAt(false);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	    
+	public void SetGazedAt(bool gazedAt) {
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		if (gazedAt && ForceGo != null) {
+			CheckDoneTime = Time.time + WatingTime;
+			ForceGo = this.gameObject;
+		} else {
+			ForceGo = null;
+		}
+	}
+
+	void Update()
+	{
+		CheckForceLook ();
+	}
+
+	public void CheckForceLook(){
+		if (ForceGo != null && Time.time>CheckDoneTime) {
+			CardBoard.transform.position = this.gameObject.transform.position;
+			ForceGo = null;
+		}
 	}
 }
