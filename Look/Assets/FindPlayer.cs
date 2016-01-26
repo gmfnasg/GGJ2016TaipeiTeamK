d0d1f000e73ff36af16304e68581410e04834ded
gmfnasg <gpx_mark_farng@hotamil.com>
2016-01-26 21:51:29 +0800

    發現玩家功能完成但須實做觸發行為

--- Look/Assets/FindPlayer.cs
+++ Look/Assets/FindPlayer.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class FindPlayer : MonoBehaviour
 {
     string SystemName = "FindPlayer";
-    public Camera Monitor;//監視攝影機
     public float VisionDistance = 10;
+    public float VisionAngel = 10;
     public bool OnLook = false;
+
+    public float LookDistance = 0;
+    public float LookAngel = 0;
+    public Vector3 LookVector = Vector3.zero;
 
     // Use this for initialization
     void Start()
     {
-        FintMonitor();
     }
 
     // Update is called once per frame
@@ -23,64 +26,99 @@ public class FindPlayer : MonoBehaviour
     void CheckOnLook()
     {
         //視線尋找法
+        if (PlayerControl.Instance == null)
+            return;
+        Vector3 playerPos = PlayerControl.Instance.gameObject.transform.position;
 
+        #region 檢查距離
+        LookDistance = Vector3.Distance(transform.position, playerPos);
+        if (LookDistance > VisionDistance)
+        {
+            if (OnLook)
+                OnDisappear();
+            OnLook = false;
+            return;
+        }
+        #endregion 檢查距離
 
-        //攝影機尋找法
-        //Renderer renderer = gameObject.GetComponent<Renderer>();
-        //if (renderer == null)
-        //    return;
+        #region 檢查障礙物
+        //取得中新點到玩家之間的向量
+        Vector3 pos = transform.position;
+        LookVector = new Vector3(playerPos.x - pos.x, playerPos.y - pos.y, playerPos.z - pos.z);
 
-        //if (!renderer.isVisible)//出現在任何一台攝影影機中，包含編輯畫面有出現也會判定為true
-        //    return;
+        bool lookPlayer = false;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, LookVector, out hit, VisionDistance))
+        {
+            lookPlayer = hit.collider.gameObject == PlayerControl.Instance.gameObject;
+        }
+        else
+        {
+            if (OnLook)
+                OnDisappear();
+            OnLook = false;
+        }
+        #endregion 檢查障礙物
 
-        //GameObject playerGo = PlayerControl.Instance.gameObject;
-        //if (playerGo == null)
-        //    return;
-        
-        //if(OnCamera.InCamera(Monitor, gameObject, VisionDistance))
-        //{
-        //    if (!OnLook)
-        //    {
-        //        DebugSystem.AddLog(DebugSystem.DebugInfo.GetNewDebugInfo(
-        //            DebugSystem.DebugInfo.DebugLogTypeEnum.Info,
-        //            SystemName,
-        //            this.gameObject.name + "發現主角"));
-        //    }
-
-        //    OnLook = true;
-        //}
-        //else
-        //{
-        //    if (OnLook)
-        //    {
-        //        DebugSystem.AddLog(DebugSystem.DebugInfo.GetNewDebugInfo(
-        //            DebugSystem.DebugInfo.DebugLogTypeEnum.Info,
-        //            SystemName,
-        //            "主角離開"+this.gameObject.name + "視線"));
-        //    }
-
-        //    OnLook = false;
-        //}
-            
-
-
-        //DebugSystem.AddLog(DebugSystem.DebugInfo.GetNewDebugInfo(
-        //DebugSystem.DebugInfo.DebugLogTypeEnum.Info,
-        //SystemName,
-        //this.gameObject.name + "被觀看"));
+        #region 檢查角度
+        if (lookPlayer)
+        {
+            LookAngel = Vector3.Angle(transform.forward, LookVector);
+            if (LookAngel < VisionAngel)
+            {
+                if (!OnLook)
+                    OnFind();
+                OnLook = true;
+            }
+            else
+            {
+                if (OnLook)
+                    OnDisappear();
+                OnLook = false;
+            }
+        }
+        else
+        {
+            if (OnLook)
+                OnDisappear();
+            OnLook = false;
+        }
+        #endregion 檢察角度
     }
 
-    void FintMonitor()
+    public void OnFind()
     {
-        if (Monitor == null)
-        {
-            Transform monitorTransform = transform.Find("Monitor");
-            if (monitorTransform == null)
-                return;
-            Monitor = monitorTransform.GetComponent<Camera>();
+        DebugSystem.AddLog(DebugSystem.DebugInfo.GetNewDebugInfo(
+            DebugSystem.DebugInfo.DebugLogTypeEnum.Info,
+            SystemName,
+            this.gameObject.name + "發現主角"));
+    }
 
-            if (Monitor == null)
-                Debug.LogError("無法找到監視器");
-        }
+    public void OnDisappear()
+    {
+        DebugSystem.AddLog(DebugSystem.DebugInfo.GetNewDebugInfo(
+           DebugSystem.DebugInfo.DebugLogTypeEnum.Info,
+           SystemName,
+          "主角離開" + this.gameObject.name + "視線"));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, transform.forward);
+
+
+
+        if (LookDistance > VisionDistance)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.blue;
+        if (OnLook)
+            Gizmos.color = Color.yellow;
+
+        if (LookAngel < VisionAngel)
+            Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(transform.position, LookVector);
     }
 }
