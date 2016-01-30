943307ec105441012da094ba71aa840ccdfd950b
Chuan Chin Danny Lai <chuanchinlai@gmail.com>
2016-01-30 16:40:59 +0800

    Opening Scripts

---
+++ Look/Assets/Scripts/Opening.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Opening : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+
+    public void DoStart()
+    {
+        Application.LoadLevel("GameScene");
+    }
+
+    public void DoExit()
+    {
+        Application.Quit();
+    }
+}
