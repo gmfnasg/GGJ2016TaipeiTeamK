47aed04be0002777aacc420d5f2f042431184642
Chuan Chin Danny Lai <chuanchinlai@gmail.com>
2016-01-30 16:41:45 +0800

    Ending Scripts

---
+++ Look/Assets/Scripts/Ending.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ending : MonoBehaviour 
+{
+    public float y_end = 0.0f;
+    public float speed = 10.0f;
+
+    RectTransform m_loadingRectTransform;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        m_loadingRectTransform = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () 
+    {
+
+        //text's new position
+        float x = 0;
+        float y = 0;
+
+        y += speed * Time.deltaTime;
+    
+        if(m_loadingRectTransform.anchoredPosition3D.y < y_end)
+        {
+            m_loadingRectTransform.Translate(new Vector3(x, y, 0));
+        }
+
+        print(y);
+    }
+}
